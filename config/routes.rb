e78f78722736f4d758c47b7bca0b0f74f71710a7
Sambalero <kevinswallow2@hotmail.com>
2012-07-16 12:30:28 -0700

    tweaking formats

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Newbies::Application.routes.draw do
 
   resources :users
 
-match '/admin' => 'visitors#admin'
+  match '/admin' => 'visitors#admin'
 
 #             root        /                            visitors#welcome
 #     visitors_home GET    /visitors/home(.:format)     visitors#home
