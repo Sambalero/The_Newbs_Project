b92ee7fffe0eb7084e583d11471a2cddfa45206d
Sambalero <kevinswallow2@hotmail.com>
2012-07-17 19:48:31 -0700

    heroku mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'app5795628@heroku.com',
-  :password       => 'aqhvp7hv',
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
