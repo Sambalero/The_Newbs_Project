2b4658c64e8c05c361c9dbef5698a2c7e895dc6d
Sambalero <kevinswallow2@hotmail.com>
2012-08-04 23:45:05 -0700

    ie working yet2?

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Newbies
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += %w( application-all.css.scss )
+    config.assets.precompile += %w( application-ie.css.scss )
   end
 end
